cc1609d7135118cc6c692369bcb5ab98a47bc9b8
Preston <preston.james.nalls@gmail.com>
2022-01-03 05:28:48 -0600

    Add `createStar` and `createStars` functions.

--- app.js
+++ app.js
@@ -4,11 +4,38 @@ const createDiv = (className) => {
   return div;
 };
 
+const createStar = (size, top, left) => {
+  const div = document.createElement("div");
+  div.setAttribute("class", "star");
+
+  div.style.width = `${size}vmin`;
+  div.style.height = `${size}vmin`;
+  div.style.top = `${top}vmin`;
+  div.style.left = `${left}vmin`;
+  
+  return div;
+};
+
+const createStars = () => {
+  const stars = document.createElement("div");
+  stars.setAttribute("class", "stars");
+  new Array(240).fill(0).forEach((value, index) => {
+    const size = Math.random() * 4;
+    const top = Math.random() * 300 - 200;
+    const left = Math.random() * 200 - 100;
+    const star = createStar(size, top, left);
+    stars.appendChild(star);
+  });
+  return stars;
+};
+
 const main = () => {
   const root = document.getElementById("root"),
     canvas = createDiv("canvas"),
-    world = createDiv("world");
+    world = createDiv("world"),
+    stars = createStars();
 
+  canvas.appendChild(stars);
   canvas.appendChild(world);
   root.appendChild(canvas);
 };
